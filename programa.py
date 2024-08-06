21d8c30324b84cc160bb24f8778277688f803a21
AnthonyR. <anthonyrobayoreyes@gmail.com>
2024-08-05 19:49:03 -0500

    Test_6.py passed

--- programa.py
+++ programa.py
@@ -42,24 +42,26 @@
 #     # Se solicita al usuario que ingrese un número
 
 # 5 Crea una función que calcule el área de un círculo dado su radio.
-import math
+# import math
 
-def area_circulo(radio):
-    area = math.pi * radio ** 2  # Se calcula el área del círculo utilizando la fórmula matemática
-    return area# Se devuelve el área calculada
-
-if __name__ == "__main__":
-    radio = float(input("Ingrese el radio del círculo: "))  # Se solicita al usuario que ingrese el radio del círculo
-    print(area_circulo(radio))# Se imprime el área calculada del círculo
-
-# # 6 Define una función que reciba dos números como argumentos y devuelva su suma.
-# def suma(a, b):
-#     # Se devuelve la suma de los dos números recibidos como argumentos
+# def area_circulo(radio):
+#     area = math.pi * radio ** 2  # Se calcula el área del círculo utilizando la fórmula matemática
+#     return area# Se devuelve el área calculada
 
 # if __name__ == "__main__":
-#     num1 = int(input("Ingrese el primer número: "))  # Se solicita al usuario que ingrese el primer número
-#     # Se solicita al usuario que ingrese el segundo número
-#     print("La suma es:", suma(num1, num2))  # Se imprime la suma de los dos números ingresados
+#     radio = float(input("Ingrese el radio del círculo: "))  # Se solicita al usuario que ingrese el radio del círculo
+#     print(area_circulo(radio))# Se imprime el área calculada del círculo
+
+# 6 Define una función que reciba dos números como argumentos y devuelva su suma.
+def suma(a, b):
+    result=a + b
+    return result# Se devuelve la suma de los dos números recibidos como argumentos
+
+if __name__ == "__main__":
+    num1 = int(input("Ingrese el primer número: "))  # Se solicita al usuario que ingrese el primer número
+    num2 = int(input("Ingrese el segundo número: ")) # Se solicita al usuario que ingrese el segundo número
+   
+    print("La suma es:", str(suma(num1, num2)))  # Se imprime la suma de los dos números ingresados
 
 # # 7 Modifica la función que calcula el área del círculo para que reciba el radio como parámetro.
 # import math
